236b5c7488b314c3558177658246e2de9dca772c
ChristopherHardman <cwhardman@gmail.com>
2020-02-26 15:54:14 +0000

    config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const UserController = require('./controllers/UserController')
+const UserController = require('./controllers/userControllers')
 const port = 3001
 
 const app = express()
@@ -9,5 +9,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.post('/signin', UserController.signIn)
+app.get('/', (req, res) => res.send('Hello World!'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
