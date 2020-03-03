86b40a3f8de993593964b933a72d0573b988185d
ChristopherHardman <cwhardman@gmail.com>
2020-03-03 17:08:25 +0000

    search and websockets

---
+++ controllers/searchControllers.js
@@ -0,0 +1,78 @@
+
+// const wss = require('../index.js')
+const WebSocket = require('ws');
+
+const Samples = [
+  {currencyPair: 'AUDUSD',
+    buySell: 'Sell',
+    dateTenor: 'O/N',
+    strike: 'ATMS',
+    vol: 12.25,
+    spotRef: 0.7101,
+    premium: 0.32,
+    liveSpot: 0.71707,
+    cp: 'C',
+    traderName: 'Olivia Evans',
+    amountFilled: '50%'
+  },
+  {currencyPair: 'EURAUD',
+    buySell: 'Buy',
+    dateTenor: 'O/N',
+    strike: '1.6025',
+    vol: 13.1,
+    spotRef: 1.6012,
+    premium: 0.28,
+    liveSpot: 0.6005,
+    cp: 'C / P',
+    traderName: 'Alfie Wood',
+    amountFilled: '75%'
+  },
+  {currencyPair: 'AUDUSD',
+    buySell: 'Sell',
+    dateTenor: 'O/N',
+    strike: 'ATMS',
+    vol: 12.25,
+    spotRef: 0.7101,
+    premium: 0.32,
+    liveSpot: 0.71707,
+    cp: 'C',
+    traderName: 'Olivia Evans',
+    amountFilled: '50%'
+  },
+  {currencyPair: 'EURAUD',
+    buySell: 'Buy',
+    dateTenor: 'O/N',
+    strike: '1.6025',
+    vol: 13.1,
+    spotRef: 1.6012,
+    premium: 0.28,
+    liveSpot: 0.6005,
+    cp: 'C / P',
+    traderName: 'Alfie Wood',
+    amountFilled: '75%'
+  },
+]
+
+
+const search = async (req, res) => {
+  try {
+    console.log('search', req.body)
+    // const wss = new WebSocket.Server({ port: 80 });
+    // wss.on('connection', function connection(ws, req) {
+    //   ws.on('message', function incoming(message) {
+    //     console.log('received: %s', message);
+    //   })
+    //   ws.send('something else');
+    //   ws.close()
+    // });
+
+    res.send(Samples)
+
+  } catch (error) {
+    console.log('ERROR', error)
+    res.sendStatus(500)
+  }
+}
+
+
+module.exports = {search};
--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const UserController = require('./controllers/userControllers')
-const port = process.env.PORT || 3000;
+const SearchController = require('./controllers/searchControllers')
+const port = process.env.PORT || 3001;
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws, req) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message, wss.clients);
+    // ws.send('something');
+    wss.clients.forEach(function each(client) {
+        client.send('something');
+  })
+})
+});
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
+
 app.post('/signin', UserController.signIn)
+app.post('/search', SearchController.search)
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
