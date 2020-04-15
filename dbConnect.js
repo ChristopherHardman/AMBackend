871719989d1ad8b384c2da4c8ae4481b569246f9
ChristopherHardman <cwhardman@gmail.com>
2020-04-15 19:18:59 +0100

    Config

--- dbConnect.js
+++ dbConnect.js
@@ -23,11 +23,11 @@ const Tracker = sequelize.import(`${__dirname}/models/trackerModel`)
 const Company = sequelize.import(`${__dirname}/models/companyModel`)
 const CustomList = sequelize.import(`${__dirname}/models/customListModel`)
 
-User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
-Axe.sync({ force: true })
-Tracker.sync({ force: true })
-Company.sync({ force: true })
-CustomList.sync({ force: true })
+// User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
+// Axe.sync({ force: true })
+// Tracker.sync({ force: true })
+// Company.sync({ force: true })
+// CustomList.sync({ force: true })
 
 // To add: deltaAmount, deltaCurrency, salesCredit
 const recordActivity = async (type, user) => {
